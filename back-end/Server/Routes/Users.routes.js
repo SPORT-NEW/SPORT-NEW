854ca0ce0dfa6f166d4031e88bdfffa06120ddec
MohamedAliFaidi <dalyyy3259@protonmail.com>
2022-09-20 17:58:13 +0100

    fixing server side issues+ adding logout user request handler

--- back-end/Server/Routes/Users.routes.js
+++ back-end/Server/Routes/Users.routes.js
@@ -1,9 +1,12 @@
 const express = require("express") ;
 const router = express.Router() ;
 
-const  { addClient , userAuthentification }=require("../Controllers/Users.controllers")
+const  { addClient , userAuthentification, logout }=require("../Controllers/Users.controllers")
 
 router.post("/signup",addClient)
 router.post("/signin",userAuthentification)
+router.get("/logout",logout)
+
+
 
 module.exports = router;
