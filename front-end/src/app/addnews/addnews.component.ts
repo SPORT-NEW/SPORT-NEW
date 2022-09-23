607783171827798decda09aa8e2bfd03c8afaf62
BahaeddineElkhalfet <102801878+Bahaeddine-GIT@users.noreply.github.com>
2022-09-23 19:11:29 +0100

    signupsuccess

--- front-end/src/app/addnews/addnews.component.ts
+++ front-end/src/app/addnews/addnews.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from '../_services/data.service';
+import { HttpClient } from '@angular/common/http';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-addnews',
@@ -8,14 +10,25 @@ import { DataService } from '../_services/data.service';
   styleUrls: ['./addnews.component.css']
 })
 export class AddnewsComponent implements OnInit {
-
-  constructor(private data: DataService, private path: Router) { }
+file:any
+files:any
+url:any
+  constructor(private data: DataService, private path: Router,private  http: HttpClient) { }
 
   ngOnInit(): void {
   }
-  newpost(f: any) {
-    console.log("f--->",f)
-    let post = f.value
+  newpost(add: any) {
+    const formData = new FormData();
+    formData.append("upload_preset", "lweb9fhl");
+    formData.append("file",this.files);
+    console.log("f--->",add)
+this.http.post(`https://api.cloudinary.com/v1_1/trust-us/upload`,formatDate).subscribe(res=>{
+  console.log("resCLD",res)
+
+})
+
+
+    let post = add.value
     console.log("newpost--->",post)
     this.data.addNew(post).subscribe(res => {
       console.log("addres--->",res)
@@ -23,5 +36,28 @@ export class AddnewsComponent implements OnInit {
     }
     )
 
+  }
+
+  fileUpload(event: any) {
+    this.file = event.target.files[0];
+    console.log("file--->",this.file);
+    
+    const reader = new FileReader();
+console.log("readres--->",reader.result)
+    if (event.target.files && event.target.files.length) {
+      const [file] = event.target.files;
+      console.log(file);
+      
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+       this.files = reader.result;
+
+        this.url = reader.result as string;
+        console.log(this.url);
+        
+
+      };
+            
+    }
   }
 }
