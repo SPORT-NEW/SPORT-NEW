539bb21f6f3e741f55074e39d943e5d1b65b9c15
MohamedAliFaidi <dalyyy3259@protonmail.com>
2022-09-24 13:20:21 +0100

    fixing dependencies

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -31,7 +31,7 @@ import { Ng2SearchPipeModule } from 'ng2-search-filter';
     HttpClientModule,
     AppRoutingModule,
     FormsModule,
-    Ng2SearchPipeModule 
+    Ng2SearchPipeModule
   ],
   providers: [
     DataService
