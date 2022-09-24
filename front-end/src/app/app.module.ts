76f0d1cfe976e8417440361ac1147cdee7d0d78b
macbook air <macbookair@Air-de-macbook.mshome.net>
2022-09-24 11:23:33 +0100

    style add form and delete serch componet

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -10,7 +10,6 @@ import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { HttpClientModule } from '@angular/common/http';
 import { DataService } from './_services/data.service';
-import { SearchComponent } from './search/search.component';
 import { AddnewsComponent } from './addnews/addnews.component';
 import { FormsModule } from '@angular/forms';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
@@ -23,7 +22,6 @@ import { Ng2SearchPipeModule } from 'ng2-search-filter';
     AboutComponent,
     LoginComponent,
     SignUpComponent,
-    SearchComponent,
     AddnewsComponent
   ],
   imports: [
